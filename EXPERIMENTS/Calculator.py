5625fe2eb84864839a14e37d0fff32bcd35bbb6a
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2017-10-07 21:42:28 -0400

    Update Calculator.py

--- EXPERIMENTS/Calculator.py
+++ EXPERIMENTS/Calculator.py
@@ -1,1 +1,45 @@
+"""This is a multi purpose calculator that can add, subtract, multiply, divide and all of the other basic functions. This calculator can also calculate your GPA and your grades. 
 
+print("""Menu:
+	1. Addition
+	2. Subtraction
+	3. Multiplication
+	4. Division
+	5. Division w/o remainder
+	6. GPA Calculator
+	7. Grade Converter
+	8. Statistics Calculator""")
+	
+calc_function = int(input("Please enter what calculator function you would like to have. "))
+while calc_function < 1 calc_function > 8:
+	calc_function = int(input("ERROR: The integer must be between 1 and 8, inclusively. "))
+
+if calc_function == 1:
+	
+Addition():
+	x1 = float(input("Please enter a value for x1 " ))
+	x2 = float(input("Please enter a value for x2. "))
+	total = x1 + x2
+	print(total)
+	
+Subtraction():
+	x1 = float(input("Please enter a value for x1 " ))
+	x2 = float(input("Please enter a value for x2. "))
+	total = x1 - x2
+	print(total)
+	
+Multiplication():
+	x1 = int(input("Please enter a value for x1 " ))
+	x2 = int(input("Please enter a value for x2. "))
+	total = x1 * x2
+	print(total)
+	
+Division_No_Remainder():
+	x1 = int(input("Please enter a value for x1 " ))
+	x2 = int(input("Please enter a value for x2. "))
+	total = x1//x2
+	print(total)
+	
+GPA_Calculator():
+Grade_Converter():
+Statistics_Calculator():
