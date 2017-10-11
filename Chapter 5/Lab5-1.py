cbd04013a12320db779f7ab1c31fbe4d00b85f3e
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2017-10-10 21:26:52 -0400

    Update Lab5-1.py

--- Chapter 5/Lab5-1.py
+++ Chapter 5/Lab5-1.py
@@ -3,11 +3,30 @@ will determine  the number of quarters, number of dimes, number of nickels, and 
 needed to make the change if you want to minimize the total number of coins used.  The number 
 of each coin needed should be sent back to the function call.
 
-Expected input"""
+Input needed: The amount of change.
+Expected output: The minimum amount of pennies nickels, dimes, and quarters required. 
+Algorithm: """
+
+pennies = 0.01
+nickels = 0.05
+dimes = 0.10
+quarters = 0.25
 
 def main():
 	money = float(input("Please enter the amount of change needed. "))
 	while money < 0:
-		money = float(input("ERROR: The amount of
+		money = float(input("ERROR: The amount of change needs to be positive."))
+	
+	exchange(	
+def exchange(money):
+	num_quarters = money // quarters
+	change1 = money % quarters
+	num_dimes = change1 // dimes
+	change2 = change1 % dimes
+	num_nickels = change2 // nickels
+	change3 = change2 // pennies
+	num_pennies = change3// pennies
+	
+	return num_quarters, num_dimes, num_nickels, num_pennies
 	
 	
