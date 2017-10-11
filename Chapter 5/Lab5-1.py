0360ffba3ab5aa0c1de30a941276415493ec2c58
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2017-10-10 20:46:37 -0400

    Update Lab5-1.py

--- Chapter 5/Lab5-1.py
+++ Chapter 5/Lab5-1.py
@@ -1,1 +1,13 @@
+""" Describe a function that will accept change needed from a purchase and 
+will determine  the number of quarters, number of dimes, number of nickels, and number of pennies 
+needed to make the change if you want to minimize the total number of coins used.  The number 
+of each coin needed should be sent back to the function call.
 
+Expected input"""
+
+def main():
+	money = float(input("Please enter the amount of change needed. "))
+	while money < 0:
+		money = float(input("ERROR: The amount of
+	
+	
