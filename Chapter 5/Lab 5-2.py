c0d0339f842fb0b6a067a7335dd2e8556865739f
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2017-10-10 22:29:56 -0400

    Update Lab 5-2.py

--- Chapter 5/Lab 5-2.py
+++ Chapter 5/Lab 5-2.py
@@ -1,20 +1,19 @@
 """Problem 2:
-  Describe a function that would accept 7 values from the function call and determine 
+Describe a function that would accept 7 values from the function call and determine 
 the minimum and maximum of those 7 values. Design this function without using the idea of 
-arrays or lists.  Send back to the function call the minimum and maximum value.  (Hint
-, you 
+arrays or lists.  Send back to the function call the minimum and maximum value. (Hint, you 
 should use no more than 7 comparisons to find the maximum and no more than 7 comparisons to 
 find the minimum.) """
 
 def main():
-x1 = float(input()) 
-x2 =float(input()) 
-x3 = float(input()) 
-x4 = float(input()) 
-x5 =float(input()) 
-x6 =float(input()) 
-x7 =float(input()) 
-x8 = float(input()) 
+  x1 = float(input("Please enter a value for x1. ")) 
+  x2 = float(input("Please enter a value for x1. ")) 
+  x3 = float(input("Please enter a value for x1. ")) 
+  x4 = float(input("Please enter a value for x1. ")) 
+  x5 = float(input("Please enter a value for x1. ")) 
+  x6 = float(input("Please enter a value for x1. ")) 
+  x7 = float(input("Please enter a value for x1. ")) 
+  x8 = float(input("Please enter a value for x1. ")) 
 
 def sort_numbers():
 
