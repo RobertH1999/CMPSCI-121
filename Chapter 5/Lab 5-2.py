29c694649a1b5da16bf9dd2fbe010184b1fe8884
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2017-10-10 22:24:33 -0400

    Update Lab 5-2.py

--- Chapter 5/Lab 5-2.py
+++ Chapter 5/Lab 5-2.py
@@ -1,1 +1,20 @@
+"""Problem 2:
+  Describe a function that would accept 7 values from the function call and determine 
+the minimum and maximum of those 7 values. Design this function without using the idea of 
+arrays or lists.  Send back to the function call the minimum and maximum value.  (Hint
+, you 
+should use no more than 7 comparisons to find the maximum and no more than 7 comparisons to 
+find the minimum.) """
+
+def main():
+x1 = float(input()) 
+x2 =float(input()) 
+x3 = float(input()) 
+x4 = float(input()) 
+x5 =float(input()) 
+x6 =float(input()) 
+x7 =float(input()) 
+x8 = float(input()) 
+
+def sort_numbers():
 
