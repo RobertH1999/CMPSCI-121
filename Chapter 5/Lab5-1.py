6b6af6d591762e8da9b8fda775e2ce20e00cefec
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2017-10-10 22:21:22 -0400

    Update Lab5-1.py

--- Chapter 5/Lab5-1.py
+++ Chapter 5/Lab5-1.py
@@ -7,26 +7,34 @@ Input needed: The amount of change.
 Expected output: The minimum amount of pennies nickels, dimes, and quarters required. 
 Algorithm: """
 
+
+
 pennies = 0.01
 nickels = 0.05
 dimes = 0.10
 quarters = 0.25
 
-def main():
-	money = float(input("Please enter the amount of change needed. "))
-	while money < 0:
-		money = float(input("ERROR: The amount of change needs to be positive."))
-	
-	exchange(num_quarters, num_dimes, num_nickels, num_pennies)	
+
+
 def exchange(money):
-	num_quarters = money // quarters
-	change1 = money % quarters
-	num_dimes = change1 // dimes
-	change2 = change1 % dimes
-	num_nickels = change2 // nickels
-	change3 = change2 // pennies
-	num_pennies = change3// pennies
-	
-	return num_quarters, num_dimes, num_nickels, num_pennies
+    num_quarters = money // quarters
+    change1 = money % quarters
+    num_dimes = change1 // dimes
+    change2 = change1 % dimes
+    num_nickels = change2 // nickels
+    change3 = change2 // pennies
+    num_pennies = change3// pennies
+
+    return num_quarters, num_dimes, num_nickels, num_pennies
+    print("The change can be converted to", num_quarters, "quarters", num_dimes, "dimes", num_nickels, "nickels", num_pennies, "pennies. ")
+    
+def main():
+    money = float(input("Please enter the amount of change needed. "))
+    while money < 0:
+        money = float(input("ERROR: The amount of change needs to be positive."))
+    exchange(money_incoins)	
+        
+main()
+
 	
 	
