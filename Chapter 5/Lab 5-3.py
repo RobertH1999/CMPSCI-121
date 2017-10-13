ff5b8e4b72b0a27639af249a46e758926dc99109
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2017-10-13 17:18:05 -0400

    Update Lab 5-3.py

--- Chapter 5/Lab 5-3.py
+++ Chapter 5/Lab 5-3.py
@@ -1,28 +1,27 @@
 """Program 3:
   Describe a function that will prompt the user to enter 50 whole numbers
- and count 
+ and count
 how many even and odd numbers were entered (
-0 should be considered an even number).  The 
-function should employ a print statement to output the results.  
+0 should be considered an even number).  The
+function should employ a print statement to output the results.
 """
 def main():
     for i in range(50):
-	number = int(input("Please enter a whole number. "))
+        number = int(input("Please enter a whole number. "))
 	while number < 0:
-	    number = int(input("ERROR: Please enter a whole number. "))
-	    
-    even, odd = even_or_odd(number)
+            number = int(input("ERROR: Please enter a whole number. "))
+
+        even, odd = even_or_odd(number)
     print(even_or_odd(number))
-		
+
 def even_or_odd(num):
-	 "))
-			
-		even_number = 0
-		odd_number = 0
-		if number % 2 == 0:
-			even_number +=1
-		else:
-			odd_number +=1
-		return even_number, odd_number	
-	
+    even_number = 0
+    odd_number = 0
+
+    if number % 2 == 0:
+        even_number +=1
+    else:
+        odd_number +=1
+    return even_number, odd_number
+
 main()
