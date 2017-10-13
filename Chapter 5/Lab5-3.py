5ea1fdbfcde56c24db5ae5f0199cb8094f866de2
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2017-10-13 12:40:50 -0400

    Update Lab5-3.py

--- Chapter 5/Lab5-3.py
+++ Chapter 5/Lab5-3.py
@@ -1,1 +1,10 @@
+"""Program 3:
+  Describe a function that will prompt the user to enter 50 whole numbers
+ and count 
+how many even and odd numbers were entered (
+0 should be considered an even number).  The 
+function should employ a print statement to output the results.  
+"""
 
+for i in range(50):
+	
