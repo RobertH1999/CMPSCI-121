a921de45c0a242f6eb1313b9ed0b8ab8d18a5ff3
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2017-10-13 12:54:19 -0400

    Update Lab 5-3.py

--- Chapter 5/Lab 5-3.py
+++ Chapter 5/Lab 5-3.py
@@ -5,8 +5,21 @@ how many even and odd numbers were entered (
 0 should be considered an even number).  The 
 function should employ a print statement to output the results.  
 """
-
-for i in range(50):
-	number = int(input("Please enter a whole number. "))
-	while number = int(input("ERROR: Please enter 
+def main():
+	num_decision = even_or_odd(num)
+	print(num_decision)
+		
+def even_or_odd(number):
+	for i in range(50):
+		number = int(input("Please enter a whole number. "))
+		while number < 0:
+			number = int(input("ERROR: Please enter a whole number. "))
+			
+		even = 0
+		odd = 0
+		if number %2 == 0:
+			even_number +=1
+		else:
+			oddnumber +=1
 	
+main()
