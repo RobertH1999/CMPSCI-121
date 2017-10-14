569c8d504e5166a08ccac1f98e1845942e3d65c6
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2017-10-14 16:16:03 -0400

    Update Finding Median.py

--- EXPERIMENTS/Finding Median.py
+++ EXPERIMENTS/Finding Median.py
@@ -2,24 +2,28 @@ def main():
     number1 = float(input("Please input a value. "))
     number2 = float(input("Please input a value. "))
     number3 = float(input("Please input a value. "))
-  
-    number1, number2, number3 = order(number1, number2, number3)
-    print(number1, number2, number3)
+    number4 = float(input("Please input a value. "))
+    number5 = float(input("Please input a value. "))
     
-def order(num1, num2, num3):
+    number1, number2, number3 = order(number1, number2, number3, number4, number5)
+    print(number1, number2, number3, number4, number5)
+    
+def order(num1, num2, num3, num4, num5):
     temp = 0
-    if num1 > num2:
-        temp = num2
-        num2 = num1
-        num1 = temp
-    if num2 > num3:
-        temp = num3
-        num3 = num2
-        num2 = temp
-    if num1 > num2:
-        temp = num2
-        num2 = num1
-        num1 = temp
-    return num1, num2, num3
+##    if num1 > num2:
+##        temp = num2
+##        num2 = num1
+##        num1 = temp
+##    if num2 > num3:
+##        temp = num3
+##        num3 = num2
+##        num2 = temp
+##    if num1 > num2:
+##        temp = num2
+##        num2 = num1
+##        num1 = temp
+
+    while num1 > num2 and num2     
+    return num1, num2, num3, num4, num5
 
 main()
