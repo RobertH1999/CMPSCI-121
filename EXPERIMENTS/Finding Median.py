8f3bfc181389fa007b775a9ce2522ae9bc366ed4
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2017-10-14 16:07:12 -0400

    Update Finding Median.py

--- EXPERIMENTS/Finding Median.py
+++ EXPERIMENTS/Finding Median.py
@@ -1,1 +1,25 @@
+def main():
+    number1 = float(input("Please input a value. "))
+    number2 = float(input("Please input a value. "))
+    number3 = float(input("Please input a value. "))
+  
+    number1, number2, number3 = order(number1, number2, number3)
+    print(number1, number2, number3)
+    
+def order(num1, num2, num3):
+    temp = 0
+    if num1 > num2:
+        temp = num2
+        num2 = num1
+        num1 = temp
+    if num2 > num3:
+        temp = num3
+        num3 = num2
+        num2 = temp
+    if num1 > num2:
+        temp = num2
+        num2 = num1
+        num1 = temp
+    return num1, num2, num3
 
+main()
