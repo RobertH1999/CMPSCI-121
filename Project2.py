f564981f961640bdcd4e6d69d3293497c21ab359
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2017-10-14 22:31:52 -0400

    Update Project2.py

--- Project2.py
+++ Project2.py
@@ -1,1 +1,10 @@
+"""
+"""
+yearlyInterestRate = float(input("What is the yearly interest rate for the credit card entered as a percent? "))
+while yearlyInterestRate < 1 or interest_rate > 30:
+    yearlyInterestRate = float(input("Error: The yearly interest rate must be between 1 and 30. "))
+previous_balance = 0
+print("The balance from the previous month was ${:,.2f}.".format(previous_balance))
 
+monthlyInterestRate = yearlyInterestRate / 12
+totalInterest = montlyInterestRate * 0.01 * previous_balance
