84008b0cd81df677089d2dc1cde998dcf3988075
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2018-02-02 08:32:02 -0500

    Update person.py

--- person.py
+++ person.py
@@ -1,1 +1,16 @@
-import 
+import datetime
+
+class Person:
+	def __init__(self, name, birthdate, phone):
+		self.name = name
+		self.birthdate = birthdate
+		self.phone = phone
+	def age(self):
+		today = datetime.date.today()
+		month, day, year = self.birthdate.split("/")
+		age = today.year - int(year)
+		
+		if today < daytime.date(int(year),int(month),int(day)):
+			age-=1
+		return age
+	
