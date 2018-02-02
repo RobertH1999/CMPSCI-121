91b0d17f7f089a3d69f8f049a34cbf709d5d376d
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2018-02-02 08:42:07 -0500

    Update person.py

--- person.py
+++ person.py
@@ -1,10 +1,11 @@
 import datetime
 
 class Person:
-	def __init__(self, name, birthdate, phone):
+	def __init__(self, name, birthdate, phone, balance=0):
 		self.name = name
 		self.birthdate = birthdate
 		self.phone = phone
+		self.balance = balance
 	def age(self):
 		today = datetime.date.today()
 		month, day, year = self.birthdate.split("/")
@@ -13,4 +14,15 @@ class Person:
 		if today < daytime.date(int(year),int(month),int(day)):
 			age-=1
 		return age
+	
+	def withdraw(self, amount):
+		if amount > self.balance:
+			return "You don't have enough money"
+		self.balance -= amount
+			
+	def deposit(self, amount):
+		return self.balance += amount
+	
+	def check_balance(self):
+		return self.balance
 	
