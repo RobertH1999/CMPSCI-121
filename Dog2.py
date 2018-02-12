8d52fd2ec3696d6c4f68dd9288b856fb866345e8
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2018-02-12 08:15:01 -0500

    Update Dog2.py

--- Dog2.py
+++ Dog2.py
@@ -1,1 +1,25 @@
-
+class Dog:
+	number_of_legs=0
+	count = 0
+	def __init__(self, name, breed, size, age):
+		self.name = name
+		self.breed = breed
+		self.size = size
+		self.age = age
+		
+	def howMany(self):
+		return Dog.count
+	def eat(self):
+		print(self.name + " is eating")
+	def sleep(self):
+		print(self.name + " is sleeping")
+	def sit(self):
+		print(self.name + " is sitting")
+	def roll_over(self):
+		print(self.name + " is rolling over")
+	def run(self):
+		print(self.name + " is running")
+	def __str__(self):
+		return "{} is a {} year old {}!".format(self.name, 
+		self.age, self.breed)
+__repr__=__str__
