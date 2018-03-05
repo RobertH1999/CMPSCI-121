a2f8cb5fa5cc1970372b24ec55b199ce1cbb4cb0
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2018-03-04 19:04:41 -0500

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,5 +1,20 @@
+import datetime
 class Bank:
-	def __init__(self, withdraw, deposit, access_information, account, ):
+	def __init__(self, name, birthdate, phone, balance = 0.0):
+		self.name = name
+		self.birthdate = birthdate
+		self.phone = phone
+		self.balance = balance
+
+	def get_age(self):
+		today=datetime.date.today()
+		month, day, year = self.birthdate.split("/")
+		age = today.year - int(year)
+		if (today.month, today.day) < (int(month),int(day)):
+			age-=1
+		return age
+		
+	
 		
 		
 	
