43e12a6b4f2e783f1798c6bf682d3986c47d64ee
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2018-03-28 08:27:16 -0400

    Update Linearsearch.py

--- Linearsearch.py
+++ Linearsearch.py
@@ -1,1 +1,12 @@
-
+def linearSearch(item, dataList):
+	for i in range(len(datalist):
+		if dataList[i] == item:
+			return True
+		return False
+				   
+def binarySearch(item, dataList):				   
+	if len(dataList) == 0:
+		return False
+             
+                   
+				   
