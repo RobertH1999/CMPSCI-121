6eb58d7d8ee28c87b45377a6e14a1d5ab57211d6
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2018-04-18 12:36:09 +0000

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,1 +1,18 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QMenu
+from PyQt5.QtGui import QIcon
+from PyQT5.QtCore import pyqtSlot
 
+clas App(QMainWindow):
+  def __init__(self):
+    super().__init__():
+      self.title = 'Window with Menu'
+      self.left = 20
+      self.top = 40
+      self.width = 640
+      self.height = 480
+      self.initUI()
+      
+   def initUI(self):
+    self.setWindowTiel(self.title)
+    
