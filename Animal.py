fb4cdfedd64486016ee05cab3bde54fb4deaaaa4
RobertH1999 <32373877+RobertH1999@users.noreply.github.com>
2018-04-23 08:12:53 -0400

    Update Animal.py

--- Animal.py
+++ Animal.py
@@ -1,4 +1,16 @@
 class Animal:
 	number_of_legs = 0
 	def__init__(self, name, age):
+		self.name = name
+		self.age = age
+		if self.age == 0:
+			self.__id = self.__createID()
+		else:
+			self.__id = self.__createID() * age
+			
+	def __createID(self):
+		return random.randint(199, 999)
+	
+	def setID(self, idnum):
+		self.__id = id nm
 		
